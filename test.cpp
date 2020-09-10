0d06fdcddf94a582ff6bf2219d37de5b4b4c85d8
fluorinedog <fluorinedog@gmail.com>
2020-09-11 00:27:17 +0800

    buggy

--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@
 
 using namespace doglib::common;
 
-constexpr int64_t Alpha_Up = 119;
+constexpr int64_t Alpha_Up = 121;
 constexpr int64_t Alpha_Down = 1889;
 constexpr int precision = 10000;
 constexpr int META_SIZEOF = 30;
@@ -18,32 +18,41 @@ const auto logAlpha = -log2f128(Alpha.get_d());
 const auto logBeta = -log2f128(Beta.get_d());
 
 std::map<int, int> meta_map;
-SeqType* global_b;
+SeqType *global_b;
+using std::vector;
+
+std::vector<int> bits2states(std::vector<bool> bits, int transaction_size) {
+    std::vector<int> states;
+    auto indicator = mpf_class(0, precision);
+    for(auto bit: bits) {
+        indicator += bit;
+        indicator /= 2;
+    }
+    // step3: calculate next states
+    std::array<int, 4> statistics = {};
+    _Float128 entropy = 0;
+    states.clear();
+    while (entropy < transaction_size + logAlpha) {
+        int multiple = mpf_class(indicator / Alpha).get_ui();
+        if (multiple < 3) {
+            states.push_back(multiple);
+            indicator -= multiple * Alpha;
+            indicator /= Alpha;
+            entropy += logAlpha;
+            statistics[multiple]++;
+        } else {
+            states.push_back(3);
+            indicator -= 3 * Alpha;
+            indicator /= Beta;
+            entropy += logBeta;
+            statistics[3]++;
+        }
+    }
+    return states;
+}
+
 
 std::pair<SeqType, SeqType> generate_a(int N, const SeqType &seq_ans) {
-//    // step 1: generate last bits
-//    int last_bits = 1024 * 4;
-//    mpf_class indicator(0, precision);
-//    for(auto i: Range(0, last_bits)) {
-//        auto index = N - i;
-//        bool bit = seq_ans[index];
-//        indicator += bit;
-//        indicator /= 2;
-//        seqA[index] = bit;
-//        seqB[index] = bit;
-//    }
-//
-//    // step 2: generate size of last trunk
-//    N -= last_bits;
-//    for(auto i: Range(0, META_SIZEOF)) {
-//        auto index = N - i;
-//        bool bit = (last_bits >> i) & 1;
-//        seqA[index] = bit;
-//        seqB[index] = false;
-//    }
-//    N -= META_SIZEOF;
-
-
     SeqType seqA(N);
     SeqType seqB(N);
     auto fill_meta = [&seqA, &seqB](int offset, int meta) {
@@ -56,13 +65,12 @@ std::pair<SeqType, SeqType> generate_a(int N, const SeqType &seq_ans) {
             seqB[index] = false;
         }
     };
-
+    int transaction_size;
     std::vector<int> states(4096, 3);
-    int transcation_size;
     for (int step = 0;; step++) {
         // step 1: fill info, calculate next indicator
-        auto indicator = mpf_class(0, precision);
-        transcation_size = states.size();
+        transaction_size = states.size();
+        vector<bool> bits;
         for (auto i: Range(states.size())) {
             auto index = N - 1 - i;
             auto state = states[i];
@@ -71,35 +79,16 @@ std::pair<SeqType, SeqType> generate_a(int N, const SeqType &seq_ans) {
             auto bitB = !(state / 2) ^bit_ans;
             seqA[index] = bitA;
             seqB[index] = bitB;
-            indicator += bitB;
-            indicator /= 2;
+            bits.push_back(bitB);
         }
-        N -= transcation_size;
+        N -= transaction_size;
 
         // step 2: fill meta
         N -= META_SIZEOF;
-        fill_meta(N, transcation_size);
+        fill_meta(N, transaction_size);
 
-        // step3: calculate next states
-        std::array<int, 4> statistics = {};
-        double entropy = 0;
-        states.clear();
-        while (entropy < transcation_size) {
-            int multiple = mpf_class(indicator / Alpha).get_ui();
-            if (multiple < 3) {
-                states.push_back(multiple);
-                indicator -= multiple * Alpha;
-                indicator /= Alpha;
-                entropy += logAlpha;
-                statistics[multiple]++;
-            } else {
-                states.push_back(3);
-                indicator -= 3 * Alpha;
-                indicator /= Beta;
-                entropy += logBeta;
-                statistics[3]++;
-            }
-        }
+        states = bits2states(bits, transaction_size);
+
         if (N <= 2 * states.size() + 2 * META_SIZEOF) {
             break;
         }
@@ -108,20 +97,31 @@ std::pair<SeqType, SeqType> generate_a(int N, const SeqType &seq_ans) {
     // step 6 set meta and other bits
     fill_meta(0, N);
 
-    for (auto i: Range(transcation_size)) {
+    for (auto i: Range(transaction_size)) {
         auto dst_index = i + META_SIZEOF;
         auto src_index = N + META_SIZEOF + i;
         seqA[dst_index] = seqB[src_index];
         seqB[dst_index] = false;
     }
 
-    for (auto index: Range(META_SIZEOF + transcation_size, N)) {
+    for (auto index: Range(META_SIZEOF + transaction_size, N)) {
         seqA[index] = seq_ans[index];
         seqB[index] = false;
     }
     return {seqA, seqB};
 }
 
+bool inc(std::vector<int> &states) {
+    for (auto &x: states) {
+        if (x < 3) {
+            ++x;
+            return false;
+        } else {
+            x = 0;
+        }
+    }
+    return true;
+}
 
 SeqType verify_b(int N, const SeqType &seqA, const SeqType &seq_std) {
     SeqType seqB(N);
@@ -130,7 +130,12 @@ SeqType verify_b(int N, const SeqType &seqA, const SeqType &seq_std) {
         assert(ack == offset);
         seqB[offset] = value;
         bool ref_bit = (*global_b)[offset];
-        assert(ref_bit == value);
+
+        if(ref_bit != value) {
+            auto str1 = std::to_string(ref_bit);
+            auto str2 = std::to_string(value);
+            throw std::runtime_error("fuck" + str1 + str2);
+        }
         ++ack;
     };
     auto getA = [&](int offset) {
@@ -182,26 +187,29 @@ SeqType verify_b(int N, const SeqType &seqA, const SeqType &seq_std) {
         }
         iter += transaction_size;
         auto indicator = mpf_class(0, precision);
-        for (auto x: states) {
-            if (x < 3) {
-                indicator *= Alpha;
-                indicator += x * Alpha;
-            } else {
-                indicator *= Beta;
-                indicator += 3 * Alpha;
+        if (false) {
+            indicator += 1;
+        } else {
+            for (auto x: states) {
+                if (x < 3) {
+                    indicator *= Alpha;
+                    indicator += x * Alpha;
+                } else {
+                    indicator *= Beta;
+                    indicator += 3 * Alpha;
+                }
             }
         }
-        indicator = 1 - indicator;
         std::vector<bool> transaction;
         for (auto i: Range(0, transaction_size)) {
             indicator *= 2;
             auto bit = indicator.get_ui();
             indicator -= bit;
-            transaction.push_back(!bit);
+            transaction.push_back(bit);
         }
 
         candidateB.clear();
-        for(bool bit: transaction) {
+        for (bool bit: transaction) {
             candidateB.push_back(bit);
         }
     }
@@ -222,7 +230,21 @@ int main() {
     auto[a, b] = generate_a(N, expected);
     global_b = &b;
     auto vb = verify_b(N, a, expected);
-    assert(b == vb);
+
+    std::vector<int> statistics(4, 0);
+    if(b != vb) {
+        throw std::runtime_error("fuck");
+    }
+    for(auto i: Range(N)) {
+        auto a_yes = expected[i] == a[i];
+        auto b_yes = expected[i] == b[i];
+        auto state = a_yes + b_yes * 2;
+        statistics[state]++;
+    }
+    for(auto i: Range(4)) {
+        double count = statistics[i];
+        std::cout << "state " << i << "->" << count << "->" << count / N << std::endl;
+    }
 }
 
 
